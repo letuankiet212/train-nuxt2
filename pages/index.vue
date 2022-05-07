452c0418bf46cdee54f95d16c1a7917a0656d919
Le Tuan Kiet <kietlt1@nal.vn>
2022-05-07 22:06:33 +0700

    [CORE] Up Emmet VS

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-for="(movie, index) in moves_list" :key="'movie' + index">
+      {{ movie }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'HomeMovie',
+  data() {
+    return {
+      moves_list: [1, 2, 3] as Array<Number>,
+    };
+  },
+});
+</script>
